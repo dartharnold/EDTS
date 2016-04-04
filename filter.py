9d3625df624cbfbf9335e470f5d0e3982d8c726b
Andy Martin <andy.martin@ultrahaptics.com>
2016-04-04 15:40:08 +0100

    More fixes to filter.py

--- filter.py
+++ filter.py
@@ -23,19 +23,22 @@ def parse_filter_string(s):
     if k in ['direction']:
       output[k] = env.data.parse_system(output[k])
     # -> int
-    if k in ['sc_distance']:
+    elif k in ['min_sc_distance', 'max_sc_distance']:
       output[k] = int(output[k])
     # -> string.upper
-    if k in ['pad']:
+    elif k in ['pad']:
       output[k] = output[k].upper()
     # -> float
-    if k in ['direction_angle']:
+    elif k in ['direction_angle', 'min_distance', 'max_distance']:
       output[k] = float(output[k])
+    else:
+      raise KeyError("Unexpected filter key provided: {0}".format(k))
 
   return output
 
 
 def filter_list(s_list, filters, limit = None, p_src_list = None):
+  src_list = None
   if p_src_list is not None:
     src_list = [s if isinstance(s, Station) else Station.none(s) for s in p_src_list]
 
@@ -64,7 +67,7 @@ def filter_list(s_list, filters, limit = None, p_src_list = None):
       else:
         log.error("Could not find system in list: \"{0}\"!".format(s))
 
-    if ('allegiance' in filters and ((not hasattr(sy, 'allegiance')) or filters['allegiance'] != sy.allegiance):
+    if ('allegiance' in filters and ((not hasattr(sy, 'allegiance')) or filters['allegiance'] != sy.allegiance)):
       continue
 
     has_stns = (hasattr(sy, 'allegiance') and sy.allegiance is not None)
@@ -78,22 +81,32 @@ def filter_list(s_list, filters, limit = None, p_src_list = None):
     if ('pad' in filters and filters['pad'] != 'L' and st.max_pad_size == 'L'):
       continue
 
-    if ('sc_distance' in filters and st.distance is not None and st.distance > filters['sc_distance']):
+    if ('min_sc_distance' in filters and (st.distance is None or st.distance < filters['min_sc_distance'])):
+      continue
+    if ('max_sc_distance' in filters and (st.distance is None or st.distance > filters['max_sc_distance'])):
       continue
 
-    dist = math.fsum([s.distance_to(start) for start in src_list])
+    dist = None
+    if src_list is not None:
+      dist = math.fsum([s.distance_to(start) for start in src_list])
 
-    if limit is None or len(asys) < limit or dist < maxdist:
+      if ('min_distance' in filters and any([s.distance_to(start) < filters['min_distance'] for start in src_list])):
+        continue
+      if ('max_distance' in filters and any([s.distance_to(start) > filters['max_distance'] for start in src_list])):
+        continue
+
+    if limit is None or len(asys) < limit or (dist is None or dist < maxdist):
       # We have a new contender; add it, sort by distance, chop to length and set the new max distance
       asys.append(s)
       # Sort the list by distance to ALL start systems
-      asys.sort(key=lambda t: math.fsum([t.distance_to(start) for start in src_list]))
+      if src_list is not None:
+        asys.sort(key=lambda t: math.fsum([t.distance_to(start) for start in src_list]))
 
       if limit is not None:
         asys = asys[0:limit]
       maxdist = max(dist, maxdist)
 
-    return asys
+  return asys
 
 
 def all_angles_within(starts, dest1, dest2, max_angle):
