d10461c60ced11d4bb4d202159ecf905dad37951
Andy Martin <andy@infinium.org.uk>
2016-05-13 00:04:02 +0100

    Corrected bad case of copypastitis

--- pgdata.py
+++ pgdata.py
@@ -173,7 +173,7 @@ c1_f2_length_overrides = {
   'q':  16,
 }
 c1_s1_f2_total_run_length = sum([c1_f2_length_overrides.get(p, c1_s1_f2_length_default) for p in c1_infixes_s1])
-c1_s2_f2_total_run_length = sum([c1_f2_length_overrides.get(p, c1_s2_f2_length_default) for p in c1_infixes_s1])
+c1_s2_f2_total_run_length = sum([c1_f2_length_overrides.get(p, c1_s2_f2_length_default) for p in c1_infixes_s2])
 
 # Welp
 c1_arbitrary_index_offset = -35
