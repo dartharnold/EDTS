20d12b2448cc1e4db0a0a1aa89f14e0a8a5bb7c6
Andy Martin <andy@infinium.org.uk>
2016-06-13 23:51:40 +0100

    Added debug logging to db.py so it's easier to see what's being executed

--- db.py
+++ db.py
@@ -42,6 +42,7 @@ def open_db(filename = default_db_file, check_version = True):
     if db_version != schema_version:
       log.warning("DB file's schema version {0} does not match the expected version {1}.".format(db_version, schema_version))
       log.warning("This may cause errors; you may wish to rebuild the database by running update.py")
+    log.debug("DB connection opened")
   return DBConnection(conn)
 
 def initialise_db(filename = default_db_file):
@@ -55,8 +56,10 @@ class DBConnection(object):
 
   def close(self):
     self._conn.close()
+    log.debug("DB connection closed")
 
   def _create_tables(self):
+    log.debug("Creating tables...")
     c = self._conn.cursor()
     c.execute('CREATE TABLE edts_info (db_version INTEGER, db_mtime INTEGER)')
     c.execute('INSERT INTO edts_info VALUES (?, ?)', (schema_version, int(time.time())))
@@ -71,35 +74,48 @@ class DBConnection(object):
     c.execute('CREATE INDEX idx_eddb_stations_sysid ON eddb_stations (system_id)')
 
     self._conn.commit()
+    log.debug("Done.")
 
   def populate_table_eddb_systems(self, systems):
     sysdata = [(int(s['id']), s['name'], float(s['x']), float(s['y']), float(s['z']), bool(s['needs_permit']), s['allegiance'], json.dumps(s)) for s in systems]
     c = self._conn.cursor()
+    log.debug("Going for INSERT INTO eddb_systems for {} systems".format(len(sysdata)))
     c.executemany('INSERT INTO eddb_systems VALUES (?, ?, ?, ?, ?, ?, ?, ?)', sysdata)
     self._conn.commit()
+    log.debug("Done.")
 
   def populate_table_eddb_stations(self, stations):
     stndata = [(int(s['id']), int(s['system_id']), s['name'], int(s['distance_to_star']) if s['distance_to_star'] is not None else None, s['type'], s['max_landing_pad_size'], json.dumps(s)) for s in stations]
     c = self._conn.cursor()
+    log.debug("Going for INSERT INTO eddb_stations for {} stations".format(len(stndata)))
     c.executemany('INSERT INTO eddb_stations VALUES (?, ?, ?, ?, ?, ?, ?)', stndata)
     self._conn.commit()
+    log.debug("Done.")
 
   def populate_table_coriolis_fsds(self, fsds):
     fsddata = [(k, json.dumps(v)) for (k, v) in fsds.items()]
     c = self._conn.cursor()
+    log.debug("Going for INSERT INTO coriolis_fsds for {} entries".format(len(fsddata)))
     c.executemany('INSERT INTO coriolis_fsds VALUES (?, ?)', fsddata)
     self._conn.commit()
+    log.debug("Done.")
 
   def retrieve_fsd_list(self):
     c = self._conn.cursor()
-    c.execute('SELECT id, data FROM coriolis_fsds')
+    cmd = 'SELECT id, data FROM coriolis_fsds'
+    log.debug("Executing: {}".format(cmd))
+    c.execute(cmd)
     results = c.fetchall()
+    log.debug("Done.")
     return dict([(k, json.loads(v)) for (k, v) in results])
 
   def get_system_by_name(self, name):
     c = self._conn.cursor()
-    c.execute('SELECT data FROM eddb_systems WHERE name = ?', (name, ))
+    cmd = 'SELECT data FROM eddb_systems WHERE name = ?'
+    log.debug("Executing: {}; name = {}".format(cmd, name))
+    c.execute(cmd, (name, ))
     result = c.fetchone()
+    log.debug("Done.")
     if result != None:
       return json.loads(result[0])
     else:
@@ -107,8 +123,11 @@ class DBConnection(object):
 
   def get_station_by_names(self, sysname, stnname):
     c = self._conn.cursor()
-    c.execute('SELECT sy.data AS sysdata, st.data AS stndata FROM eddb_systems sy, eddb_stations st WHERE sy.name = ? AND st.name = ? AND sy.id = st.system_id', (sysname, stnname))
+    cmd = 'SELECT sy.data AS sysdata, st.data AS stndata FROM eddb_systems sy, eddb_stations st WHERE sy.name = ? AND st.name = ? AND sy.id = st.system_id'
+    log.debug("Executing: {}; sysname = {}, stnname = {}".format(cmd, sysname, stnname))
+    c.execute(cmd, (sysname, stnname))
     result = c.fetchone()
+    log.debug("Done.")
     if result != None:
       return (json.loads(result[0]), json.loads(result[1]))
     else:
@@ -116,22 +135,31 @@ class DBConnection(object):
 
   def get_stations_by_system_id(self, sysid):
     c = self._conn.cursor()
-    c.execute('SELECT data FROM eddb_stations WHERE system_id = ?', (sysid, ))
+    cmd = 'SELECT data FROM eddb_stations WHERE system_id = ?'
+    log.debug("Executing: {}; sysid = {}".format(cmd, sysid))
+    c.execute(cmd, (sysid, ))
     results = c.fetchall()
+    log.debug("Done.")
     return [json.loads(r[0]) for r in results]
 
   def get_systems_by_aabb(self, min_x, min_y, min_z, max_x, max_y, max_z):
     c = self._conn.cursor()
-    c.execute('SELECT data FROM eddb_systems WHERE ? <= pos_x AND pos_x < ? AND ? <= pos_y AND pos_y < ? AND ? <= pos_z AND pos_z < ?', (min_x, max_x, min_y, max_y, min_z, max_z))
+    cmd = 'SELECT data FROM eddb_systems WHERE ? <= pos_x AND pos_x < ? AND ? <= pos_y AND pos_y < ? AND ? <= pos_z AND pos_z < ?'
+    log.debug("Executing: {}; min_x = {}, max_x = {}, min_y = {}, max_y = {}, min_z = {}, max_z = {}".format(cmd, min_x, max_x, min_y, max_y, min_z, max_z))
+    c.execute(cmd, (min_x, max_x, min_y, max_y, min_z, max_z))
     results = c.fetchall()
+    log.debug("Done.")
     return [json.loads(r[0]) for r in results]
     
   def find_systems_by_name(self, name, mode=FIND_EXACT):
     if mode == FIND_GLOB and _find_operators[mode] == 'LIKE':
       name = name.replace('*','%').replace('?','_')
     c = self._conn.cursor()
-    c.execute('SELECT data FROM eddb_systems WHERE name {0} ?'.format(_find_operators[mode]), (name, ))
+    cmd = 'SELECT data FROM eddb_systems WHERE name {0} ?'.format(_find_operators[mode])
+    log.debug("Executing: {}; name = {}".format(cmd, name))
+    c.execute(cmd, (name, ))
     result = c.fetchone()
+    log.debug("Done.")
     while result is not None:
       yield json.loads(result[0])
       result = c.fetchone()
@@ -140,8 +168,11 @@ class DBConnection(object):
     if mode == FIND_GLOB and _find_operators[mode] == 'LIKE':
       name = name.replace('*','%').replace('?','_')
     c = self._conn.cursor()
-    c.execute('SELECT sy.data AS sysdata, st.data AS stndata FROM eddb_systems sy, eddb_stations st WHERE st.name {0} ? AND sy.id = st.system_id'.format(_find_operators[mode]), (name, ))
+    cmd = 'SELECT sy.data AS sysdata, st.data AS stndata FROM eddb_systems sy, eddb_stations st WHERE st.name {0} ? AND sy.id = st.system_id'.format(_find_operators[mode])
+    log.debug("Executing: {}; name = {}".format(cmd, name))
+    c.execute(cmd, (name, ))
     result = c.fetchone()
+    log.debug("Done.")
     while result is not None:
       yield (json.loads(result[0]), json.loads(result[1]))
       result = c.fetchone()
@@ -158,8 +189,11 @@ class DBConnection(object):
     name = _bad_char_regex.sub("", name)
     name = name.replace("'", r"''")
     c = self._conn.cursor()
-    c.execute("SELECT data FROM eddb_systems WHERE name {0} '{1}'".format(_find_operators[mode], name))
+    cmd = "SELECT data FROM eddb_systems WHERE name {0} '{1}'".format(_find_operators[mode], name)
+    log.debug("Executing (U): {}".format(cmd))
+    c.execute(cmd)
     result = c.fetchone()
+    log.debug("Done.")
     while result is not None:
       yield json.loads(result[0])
       result = c.fetchone()
@@ -170,8 +204,11 @@ class DBConnection(object):
     name = _bad_char_regex.sub("", name)
     name = name.replace("'", r"''")
     c = self._conn.cursor()
-    c.execute("SELECT sy.data AS sysdata, st.data AS stndata FROM eddb_systems sy, eddb_stations st WHERE st.name {0} '{1}' AND sy.id = st.system_id".format(_find_operators[mode], name))
+    cmd = "SELECT sy.data AS sysdata, st.data AS stndata FROM eddb_systems sy, eddb_stations st WHERE st.name {0} '{1}' AND sy.id = st.system_id".format(_find_operators[mode], name)
+    log.debug("Executing (U): {}".format(cmd))
+    c.execute(cmd)
     result = c.fetchone()
+    log.debug("Done.")
     while result is not None:
       yield (json.loads(result[0]), json.loads(result[1]))
       result = c.fetchone()
@@ -179,8 +216,11 @@ class DBConnection(object):
   # Slow as sin; avoid if at all possible
   def get_all_systems(self):
     c = self._conn.cursor()
-    c.execute('SELECT data FROM eddb_systems')
+    cmd = 'SELECT data FROM eddb_systems'
+    log.debug("Executing: {}".format(cmd))
+    c.execute(cmd)
     result = c.fetchone()
+    log.debug("Done.")
     while result is not None:
       yield json.loads(result[0])
       result = c.fetchone()
