9174d26d088eeeea926679804b16fbf7ba025a8a
CMDR furrycat <elite@furrycat.net>
2016-08-04 13:38:52 +0100

    Wrap JSON imports in a function.
    Use import_json() to handle reading, parsing and storing JSON dumps.

--- update.py
+++ update.py
@@ -18,6 +18,29 @@ eddb_stations_url = "https://eddb.io/archive/v4/stations.json"
 
 coriolis_fsds_url = "https://raw.githubusercontent.com/cmmcleod/coriolis-data/master/modules/standard/frame_shift_drive.json"
 
+def import_json(url, description, fn):
+  log.info("Downloading {0} list from {1} ... ".format(description, url))
+  sys.stdout.flush()
+  encoded = util.read_from_url(url)
+  log.info("Done.")
+  log.info("Loading {0} data...".format(description))
+  sys.stdout.flush()
+  obj = json.loads(encoded)
+  log.info("Done.")
+  log.info("Adding {0} data to DB...".format(description))
+  sys.stdout.flush()
+  fn(obj)
+  log.info("Done.")
+  # Force GC collection to try to avoid memory errors
+  encoded = None
+  obj = None
+  gc.collect()
+
+def import_coriolis_data(coriolis_fsds_obj):
+  fsddata = {}
+  for entry in coriolis_fsds_obj['fsd']:
+    fsddata['{0}{1}'.format(entry['class'], entry['rating'])] = entry
+  dbc.populate_table_coriolis_fsds(fsddata)
 
 # If the data directory doesn't exist, make it
 if not os.path.exists(os.path.dirname(db.default_db_file)):
@@ -32,75 +55,10 @@ if os.path.isfile(db_tmp_filename):
 dbc = db.initialise_db(db_tmp_filename)
 log.info("Done.")
 
-log.info("Downloading EDSM Systems list from {0} ... ".format(edsm_systems_url))
-sys.stdout.flush()
-edsm_systems_json = util.read_from_url(edsm_systems_url)
-log.info("Done.")
-log.info("Loading EDSM Systems data...")
-sys.stdout.flush()
-edsm_systems_obj = json.loads(edsm_systems_json)
-log.info("Done.")
-log.info("Adding EDSM Systems data to DB...")
-sys.stdout.flush()
-dbc.populate_table_systems(edsm_systems_obj)
-log.info("Done.")
-
-# Force GC collection to try to avoid memory errors
-edsm_systems_json = None
-edsm_systems_obj = None
-gc.collect()
-
-log.info("Downloading EDDB Systems list from {0} ... ".format(eddb_systems_url))
-sys.stdout.flush()
-eddb_systems_json = util.read_from_url(eddb_systems_url)
-log.info("Done.")
-log.info("Loading EDDB Systems data...")
-sys.stdout.flush()
-eddb_systems_obj = json.loads(eddb_systems_json)
-log.info("Done.")
-log.info("Adding EDDB Systems data to DB...")
-sys.stdout.flush()
-dbc.update_table_systems(eddb_systems_obj)
-log.info("Done.")
-
-# Force GC collection to try to avoid memory errors
-eddb_systems_json = None
-eddb_systems_obj = None
-gc.collect()
-
-log.info("Downloading EDDB Stations list from {0} ... ".format(eddb_stations_url))
-sys.stdout.flush()
-eddb_stations_json = util.read_from_url(eddb_stations_url)
-log.info("Done.")
-log.info("Loading EDDB Stations data...")
-sys.stdout.flush()
-eddb_stations_obj = json.loads(eddb_stations_json)
-log.info("Done.")
-log.info("Adding EDDB Stations data to DB...")
-sys.stdout.flush()
-dbc.populate_table_stations(eddb_stations_obj)
-log.info("Done.")
-
-# Force GC collection to try to avoid memory errors
-eddb_stations_json = None
-eddb_stations_obj = None
-gc.collect()
-
-log.info("Downloading Coriolis FSD list from {0} ... ".format(coriolis_fsds_url))
-sys.stdout.flush()
-coriolis_fsds_json = util.read_from_url(coriolis_fsds_url)
-log.info("Done.")
-log.info("Loading Coriolis FSD data...")
-sys.stdout.flush()
-coriolis_fsds_obj = json.loads(coriolis_fsds_json)
-log.info("Done.")
-log.info("Adding Coriolis FSD data to DB...")
-sys.stdout.flush()
-fsddata = {}
-for entry in coriolis_fsds_obj['fsd']:
-  fsddata['{0}{1}'.format(entry['class'], entry['rating'])] = entry
-dbc.populate_table_coriolis_fsds(fsddata)
-log.info("Done.")
+import_json(edsm_systems_url, 'EDSM Systems', dbc.populate_table_systems)
+import_json(eddb_systems_url, 'EDDB Systems', dbc.update_table_systems)
+import_json(eddb_stations_url, 'EDDB Stations', dbc.populate_table_stations)
+import_json(coriolis_fsds_url, 'Coriolis FSD', import_coriolis_data)
 
 dbc.close()
 
