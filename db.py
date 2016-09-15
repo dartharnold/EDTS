e172f53e7dd61d8ed5984fc16aa7f84f0de7a3ad
Andy Martin <andy@infinium.org.uk>
2016-09-15 16:13:21 +0100

    Small style cleanup

--- db.py
+++ db.py
@@ -156,7 +156,7 @@ class DBConnection(object):
     c.execute(cmd, (name, ))
     result = c.fetchone()
     log.debug("Done.")
-    if result != None:
+    if result is not None:
       return _process_system_result(result)
     else:
       return None
@@ -168,7 +168,7 @@ class DBConnection(object):
     c.execute(cmd, (sysname, stnname))
     result = c.fetchone()
     log.debug("Done.")
-    if result != None:
+    if result is not None:
       return (_process_system_result(result), json.loads(result['stndata']))
     else:
       return (None, None)
@@ -180,7 +180,7 @@ class DBConnection(object):
     log.debug("Executing: {}; sysid = {}".format(cmd, sysids))
     c.execute(cmd, sysids)
     results = c.fetchall()
-    log.debug("Done.")
+    log.debug("Done, {} results.".format(len(results)))
     return [{ k: v for d in [{ 'eddb_system_id': r[0] }, json.loads(r[1])] for k, v in d.items()} for r in results]
 
   def get_systems_by_aabb(self, min_x, min_y, min_z, max_x, max_y, max_z):
@@ -189,7 +189,7 @@ class DBConnection(object):
     log.debug("Executing: {}; min_x = {}, max_x = {}, min_y = {}, max_y = {}, min_z = {}, max_z = {}".format(cmd, min_x, max_x, min_y, max_y, min_z, max_z))
     c.execute(cmd, (min_x, max_x, min_y, max_y, min_z, max_z))
     results = c.fetchall()
-    log.debug("Done.")
+    log.debug("Done, {} results.".format(len(results)))
     return [_process_system_result(r) for r in results]
 
   def find_systems_close_to(self, refs):
@@ -237,7 +237,7 @@ class DBConnection(object):
 
     c.execute(cmd, params)
     results = c.fetchall()
-    log.debug("Done")
+    log.debug("Done, {} results.".format(len(results)))
     return [_process_system_result(r) for r in results]
 
     
