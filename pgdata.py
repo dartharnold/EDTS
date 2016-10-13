d8dcebde98ce82f263cd24be853b2d67db0c5223
Andy Martin <andy@infinium.org.uk>
2016-10-13 12:44:48 +0100

    Fixed omissions of punctuation in PG system name regex

--- pgdata.py
+++ pgdata.py
@@ -11,7 +11,7 @@ import vector3
 
 
 # This does not validate sector names, just ensures that it matches the 'Something AB-C d1' or 'Something AB-C d1-23' format
-pg_system_regex = re.compile('^(?P<sector>[\\w\\s]+) (?P<l1>[A-Za-z])(?P<l2>[A-Za-z])-(?P<l3>[A-Za-z]) (?P<mcode>[A-Za-z])(?:(?P<n1>\\d+)-)?(?P<n2>\\d+)$')
+pg_system_regex = re.compile("^(?P<sector>[\\w\\s'.()/-]+) (?P<l1>[A-Za-z])(?P<l2>[A-Za-z])-(?P<l3>[A-Za-z]) (?P<mcode>[A-Za-z])(?:(?P<n1>\\d+)-)?(?P<n2>\\d+)$")
 
 
 # Hopefully-complete list of valid name fragments / phonemes
