e7c39a32a70b40006a23968d9581cff59349e00f
Andy Martin <andy@infinium.org.uk>
2016-10-28 21:14:22 +0100

    Added get_modified method to Ship

--- ship.py
+++ ship.py
@@ -26,3 +26,25 @@ class Ship(object):
 
   def fuel_weight_range(self, dist, cargo = 0):
     return self.fsd.fuel_weight_range(dist, self.mass, cargo)
+
+  def get_modified(self, optmass = None, optmass_percent = None, maxfuel = None, maxfuel_percent = None, fsdmass = None, fsdmass_percent = None):
+    if (optmass is not None and optmass_percent is not None):
+      raise ValueError("A maximum of one of optmass and optmass_percent must be provided")
+    if (fsdmass is not None and fsdmass_percent is not None):
+      raise ValueError("A maximum of one of fsdmass and fsdmass_percent must be provided")
+    if (maxfuel is not None and maxfuel_percent is not None):
+      raise ValueError("A maximum of one of maxfuel and maxfuel_percent must be provided")
+    s = Ship(self.fsd.drive, self.mass, self.tank_size, self.cargo_capacity)
+    if optmass is not None:
+      s.fsd.optmass = optmass
+    elif optmass_percent is not None:
+      s.fsd.optmass *= (1.0 + optmass_percent/100.0)
+    if fsdmass is not None:
+      s.mass += (fsdmass - s.fsd.mass)
+    elif fsdmass_percent is not None:
+      s.mass += s.fsd.mass * (fsdmass_percent/100.0)
+    if maxfuel is not None:
+      s.fsd.maxfuel = maxfuel
+    elif maxfuel_percent is not None:
+      s.fsd.maxfuel *= (1.0 + maxfuel_percent/100.0)
+    return s
