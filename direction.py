68337d989fdef8bdbee575233e7c9a211f3272b6
CMDR furrycat <elite@furrycat.net>
2017-01-25 15:19:50 +0000

    Check if two systems are in the same direction.
    Use --check system1 system2 to check if system1 and system2 are
    generally in the same direction from the reference.
    Use --tolerance to set a tolerance percentage for the check, 15% by
    default.

--- direction.py
+++ direction.py
@@ -4,6 +4,7 @@ from __future__ import print_function
 import argparse
 import env
 import logging
+import sys
 import vector3
 
 app_name = "direction"
@@ -16,11 +17,17 @@ class Application(object):
     ap_parents = [env.arg_parser] if not hosted else []
     ap = argparse.ArgumentParser(description = "Calculate direction between two systems", fromfile_prefix_chars="@", parents = ap_parents, prog = app_name)
     ap.add_argument("-a", "--angle", default=False, action='store_true', help="Return angle not vector")
+    ap.add_argument("-c", "--check", default=False, action='store_true', help="Check if second system is in the same direction as the first from the reference")
     ap.add_argument("-n", "--normal", default=False, action='store_true', help="Return normalised direction vector")
     ap.add_argument("-r", "--reference", metavar="system", nargs='?', default="Sol", help="Reference system for angle calculation")
+    ap.add_argument("-t", "--tolerance", type=int, default=15, help="Tolerance in percent for --check")
     ap.add_argument("systems", metavar="system", nargs=2, help="Systems")
 
     self.args = ap.parse_args(arg)
+    if self.args.tolerance is not None:
+      if self.args.tolerance < 0 or self.args.tolerance > 100:
+        log.error("Tolerance must be in range 0 to 100 (percent)!")
+        sys.exit(1)
 
   def run(self):
     with env.use() as envdata:
@@ -37,16 +44,30 @@ class Application(object):
           return
 
       a, b = [systems[y].position for y in self.args.systems]
-      v = b - a
-      log.debug("From {} to {}".format(a, b))
-
-      if self.args.angle:
-        print(v.angle_to(a - reference.position))
-      elif self.args.normal:
-        log.debug("Normalising {}".format(v))
-        print(v.get_normalised())
+      if self.args.check:
+        v = (a - reference.position).get_normalised()
+        w = (b - reference.position).get_normalised()
+        d = v.dot(w)
+        log.debug('{0} vs {1} dot {2}'.format(v, w, d))
+        if d >= 1.0 - float(self.args.tolerance) / 100:
+          print('OK {}% deviation'.format(100 - int(100 * d)))
+          sys.exit(0)
+        elif d < 0.0:
+          print('NO opposite')
+        else:
+          print('NO {}% deviation'.format(100 - int(100 * d)))
+        sys.exit(100)
       else:
-        print(v)
+        v = b - a
+        log.debug("From {0} to {1}".format(a, b))
+
+        if self.args.angle:
+          print(v.angle_to(a - reference.position))
+        elif self.args.normal:
+          log.debug("Normalising {0}".format(v))
+          print(v.get_normalised())
+        else:
+          print(v)
 
 if __name__ == '__main__':
   env.start()
