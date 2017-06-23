e03377c2b8d66801a8bb46dfb42284f6001374de
Andy Martin <andy@infinium.org.uk>
2017-06-23 14:02:23 +0100

    Changed PG system regex to be a literal-string, resulting in less escaping

--- edtslib/pgdata.py
+++ edtslib/pgdata.py
@@ -12,7 +12,7 @@ from . import vector3
 
 
 # This does not validate sector names, just ensures that it matches the 'Something AB-C d1' or 'Something AB-C d1-23' format
-pg_system_regex = re.compile("^(?P<sector>[\\w\\s'.()/-]+) (?P<l1>[A-Za-z])(?P<l2>[A-Za-z])-(?P<l3>[A-Za-z]) (?P<mcode>[A-Za-z])(?:(?P<n1>\\d+)-)?(?P<n2>\\d+)$")
+pg_system_regex = re.compile(r"^(?P<sector>[\w\s'.()/-]+) (?P<l1>[A-Za-z])(?P<l2>[A-Za-z])-(?P<l3>[A-Za-z]) (?P<mcode>[A-Za-z])(?:(?P<n1>\d+)-)?(?P<n2>\d+)$")
 
 
 # Hopefully-complete list of valid name fragments / phonemes
