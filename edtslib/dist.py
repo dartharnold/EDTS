b67b92a0afcdf9f513ca6cdd97078c5caaeab421
CMDR furrycat <elite@furrycat.net>
2017-07-25 14:42:16 +0100

    Class to pretty print distances.
    Instantiate a generic Dist() with magnitude in metres or specific
    Metres(), Kilometres(), Megametres(), Lightseconds() or Lightyears().
    
    Call to_string() to get a pretty representation of the distance.
    
    eg Lightyears(0.22).to_string() returns '0.22Ly'.
    
    Cast to str to get a friendly representation regardless of the magnitude
    involved.
    
    eg str(Metres(299792000)) returns '0.1Ls'
    
    The difference between __str__() and to_string() being that to_string()
    always uses the suffix used when instantiating the class.
    
    eg Metres(299792000).to_string() returns '299792000m'.
    
    Finally the metres property always returns the (numeric) distance in metres.

---
+++ edtslib/dist.py
@@ -0,0 +1,70 @@
+class Dist(object):
+  KM = 1000
+  MM = 1000 * KM
+  LS = 299.792 * MM
+  LY = 31557600 * LS
+  M_SUFFIX  = 'm'
+  KM_SUFFIX = 'km'
+  MM_SUFFIX = 'Mm'
+  LS_SUFFIX = 'Ls'
+  LY_SUFFIX = 'Ly'
+
+  def __init__(self, d, s = None):
+    self.metres = d
+    self.suffix = s
+
+  def prettyprint(self, f, suffix = ''):
+    return '{:.2f}'.format(f).rstrip('0').rstrip('.') + suffix
+
+  def __str__(self):
+    if self.metres > 0.1 * self.LY:
+      return self.prettyprint(self.metres / self.LY, self.LY_SUFFIX)
+    if self.metres > 0.1 * self.LS:
+      return self.prettyprint(self.metres / self.LS, self.LS_SUFFIX)
+    if self.metres > 0.1 * self.MM:
+      return self.prettyprint(self.metres / self.MM, self.MM_SUFFIX)
+    if self.metres > self.KM:
+      return self.prettyprint(self.metres / self.KM, self.KM_SUFFIX)
+    return self.prettyprint(self.metres, self.M_SUFFIX)
+
+  def to_string(self):
+    return str(self)
+
+class Metres(Dist):
+  def __init__(self, d):
+    super(Metres, self).__init__(d, self.M_SUFFIX)
+
+  def to_string(self):
+    return self.prettyprint(self.metres, self.suffix)
+
+class Kilometres(Dist):
+  def __init__(self, d):
+    super(Kilometres, self).__init__(d * self.KM, self.KM_SUFFIX)
+    self.kilometres = d
+
+  def to_string(self):
+    return self.prettyprint(self.kilometres, self.suffix)
+
+class Megametres(Dist):
+  def __init__(self, d):
+    super(Megametres, self).__init__(d * self.MM, self.MM_SUFFIX)
+    self.megametres = d
+
+  def to_string(self):
+    return self.prettyprint(self.megametres, self.suffix)
+
+class Lightseconds(Dist):
+  def __init__(self, d):
+    super(Lightseconds, self).__init__(d * self.LS, self.LS_SUFFIX)
+    self.lightseconds = d
+
+  def to_string(self):
+    return self.prettyprint(self.lightseconds, self.suffix)
+
+class Lightyears(Dist):
+  def __init__(self, d):
+    super(Lightyears, self).__init__(d * self.LY, self.LY_SUFFIX)
+    self.lightyears = d
+
+  def to_string(self):
+    return self.prettyprint(self.lightyears, self.suffix)
---
+++ test/unit/test_dist.py
@@ -0,0 +1,27 @@
+import unittest
+import sys
+
+sys.path.insert(0, '../..')
+from edtslib import dist
+del sys.path[0]
+
+class TestDist(unittest.TestCase):
+  def test_km(self):
+    d = dist.Kilometres(1.5)
+    self.assertEqual(d.metres, 1500)
+    self.assertEqual(d.to_string(), "1.5km")
+
+  def test_mm(self):
+    d = dist.Megametres(0.1)
+    self.assertEqual(d.metres, 100000)
+    self.assertEqual(d.to_string(), "0.1Mm")
+
+  def test_ls(self):
+    d = dist.Lightseconds(1)
+    self.assertEqual(d.metres, 299792000)
+    self.assertEqual(d.to_string(), "1Ls")
+
+  def test_ly(self):
+    d = dist.Lightyears(0.22)
+    self.assertAlmostEqual(d.metres, 0.22 * 31557600 * 299.792 * 1000000, delta=2)
+    self.assertEqual(d.to_string(), "0.22Ly")
