a980c998f7f1ef786ac4326aaf84fb2af7f38a91
CMDR furrycat <elite@furrycat.net>
2017-07-25 14:20:05 +0100

    ColumnObjectWriter class.
    Use it to format rows nicely with aligned columns.
    
    Instantiate the class:
    
      cow = ColumnObjectWriter()
    
    Optionally provide the number of columns.
    
      cow = ColumnObjectWriter(3)
    
    Optionally provide a list of alignments '<' or '>'.
    
      cow = ColumnObjectWriter(3, ['<', '>', '<'])
    
    The first and third columns will be left justified.  The second will be
    right justified.
    
    Add rows.
    
      col.add(['row 1 column 1', 'row 1 column 2', 'row 1 column 3'])
    
    Print it!
    
      col.out()
    
    Or iterate the rows:
    
      for row in col.render():
        print(row)
    
    Or convert the whole table to_string().

---
+++ edtslib/cow.py
@@ -0,0 +1,69 @@
+import collections
+import util
+
+class ColumnObjectWriter(object):
+  def __init__(self, columns = 0, padding = None):
+    self._rows = []
+    self._lengths = []
+    self._padding = []
+    self.fmt = ''
+    self.intra = None
+    self.columns = 0
+    self.rows = 0
+    self.expand(columns, padding)
+
+  def expand(self, columns, padding = None):
+    if padding is None:
+      padding = '<'
+    if columns > 0:
+      self._lengths += [0 for column in range(0, columns)]
+      if isinstance(padding, collections.Iterable) and not util.is_str(padding):
+        padding += [padding[-1] for column in range(0, columns - len(padding))]
+        self._padding += padding
+      else:
+        self._padding += [padding for column in range(0, columns)]
+      self.columns += columns
+
+  def size(self, column):
+    return self._lengths[column]
+
+  def add(self, row):
+    columns = len(row)
+    self.expand(columns - self.columns)
+    s = []
+    reformat = False
+    for column in range(0, columns):
+      content = str(row[column])
+      s.append(content)
+      l = len(content)
+      o = self._lengths[column]
+      reformat |= (l != o)
+      self._lengths[column] = max(l, o)
+    self._rows.append(s)
+    self.rows += 1
+    if reformat:
+      self.reformat()
+
+  def reformat(self, intra = None):
+    if intra is None:
+      intra = '  '
+    self.intra = intra
+    self.fmt = self.intra.join(['{}'.format(' ' if column > 0 else '') + '{: ' + self._padding[column] + str(self._lengths[column]) + '}' for column in range(0, self.columns)])
+
+  def render(self, intra = None):
+    if not self.fmt or (intra is not None and intra != self.intra):
+      self.reformat()
+    for row in self._rows:
+      yield self.fmt.format(*row)
+
+  def to_string(self, intra = None):
+    s = ''
+    for row in self.render(intra):
+      if len(s):
+        s += "\n"
+      s += row
+    return s
+
+  def out(self, intra = None):
+    for row in self.render(intra):
+      print(row)
