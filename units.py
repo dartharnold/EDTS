bdb1196a3717fdae1517f0eb40ef3a6984f7b920
Andy Martin <andy.martin@ultrahaptics.com>
2017-08-09 12:12:58 +0100

    Remove env start/stop from units, as it doesn't need the DB for anything

--- units.py
+++ units.py
@@ -6,7 +6,5 @@ from edtslib import units
 
 if __name__ == '__main__':
   env.configure_logging(env.global_args.log_level)
-  env.start()
   a = units.Application(env.local_args, False)
   a.run()
-  env.stop()
