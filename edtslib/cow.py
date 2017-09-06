df9d43347686268307fb695d2a7359a8b9f24817
CMDR furrycat <elite@furrycat.net>
2017-09-06 17:01:19 +0100

    Allow variable padding between COW columns.
    Columns are separated with three spaces by default.  A single separator
    or list of separators can be passed to the constructor.
    
    Separators can be overridden when reformatting.

--- edtslib/cow.py
+++ edtslib/cow.py
@@ -2,26 +2,32 @@ import collections
 from . import util
 
 class ColumnObjectWriter(object):
-  def __init__(self, columns = 0, padding = None):
+  def __init__(self, columns = 0, padding = None, separator = None):
     self._rows = []
     self._lengths = []
     self._padding = []
+    self._intra = []
     self.fmt = ''
-    self.intra = None
     self.columns = 0
     self.rows = 0
-    self.expand(columns, padding)
+    self.expand(columns, padding, separator)
 
-  def expand(self, columns, padding = None):
+  def _expand_parameter(self, columns, parameter, values):
+    if isinstance(values, collections.Iterable) and not util.is_str(values):
+      values += [values[-1] for column in range(0, columns - len(values))]
+      parameter += values
+    else:
+      parameter += [values for column in range(0, columns)]
+
+  def expand(self, columns, padding = None, intra = None):
     if padding is None:
-      padding = '<'
+      padding = self._padding[-1] if len(self._padding) else '<'
+    if intra is None:
+      intra = self._intra[-1] if len(self._intra) else '   '
     if columns > 0:
       self._lengths += [0 for column in range(0, columns)]
-      if isinstance(padding, collections.Iterable) and not util.is_str(padding):
-        padding += [padding[-1] for column in range(0, columns - len(padding))]
-        self._padding += padding
-      else:
-        self._padding += [padding for column in range(0, columns)]
+      self._expand_parameter(columns, self._padding, padding)
+      self._expand_parameter(columns, self._intra, intra)
       self.columns += columns
 
   def size(self, column):
@@ -46,13 +52,15 @@ class ColumnObjectWriter(object):
 
   def reformat(self, intra = None):
     if intra is None:
-      intra = '  '
-    self.intra = intra
-    self.fmt = self.intra.join(['{}'.format(' ' if column > 0 else '') + '{: ' + self._padding[column] + str(self._lengths[column]) + '}' for column in range(0, self.columns)])
+      _intra = self._intra
+    else:
+      _intra = []
+      self._expand_parameter(self.columns, _intra, intra)
+    self.fmt = ''.join(['{}'.format(_intra[column - 1] if column > 0 else '') + '{: ' + self._padding[column] + str(self._lengths[column]) + '}' for column in range(0, self.columns)])
 
   def render(self, intra = None):
-    if not self.fmt or (intra is not None and intra != self.intra):
-      self.reformat()
+    if not self.fmt or (intra is not None and intra != self._intra):
+      self.reformat(intra)
     for row in self._rows:
       yield self.fmt.format(*row)
 
